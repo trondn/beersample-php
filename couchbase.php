a07e217f4d25674eeef8f6bf81f2626f0bfa7fa8
Trond Norbye <trond.norbye@gmail.com>
2012-12-10 23:53:25 +0100

    Use 127.0.0.1 by default

--- couchbase.php
+++ couchbase.php
@@ -2,7 +2,7 @@
 /* This file contains the credentials used to access
  * the Couchbase cluster.
  */
-define("COUCHBASE_CONFIG_HOST", "10.0.0.33");
+define("COUCHBASE_CONFIG_HOST", "127.0.0.1");
 define("COUCHBASE_CONFIG_USER", "");
 define("COUCHBASE_CONFIG_PASSWD", "");
 define("COUCHBASE_CONFIG_BUCKET", "beer-sample");
